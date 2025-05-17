7bd018760450e3737db263784622eb689c98eb1d
CCEPCAHER <franklin_carranza@hotmail.com>
2025-05-17 21:13:16 +0200

    Actualizar sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,21 @@
-// sw.js
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open("app-cache").then(cache => {
-      return cache.addAll([
-        "/",                  // para index.html
+    caches.open("app-cache").then(cache =>
+      cache.addAll([
+        "/", 
         "/index.html",
         "/style.css",
         "/script.js",
         "/manifest.json",
         "/icons/icon-192x192.png",
         "/icons/icon-512x512.png"
-      ]);
-    })
+      ])
+    )
   );
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(resp => resp || fetch(event.request))
   );
 });
